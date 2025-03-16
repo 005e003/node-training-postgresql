56d4aa5c5dff3c61ce98b903ef72277b6fc09eb9
NoraHsu <p0938832290@gmail.com>
2025-03-16 23:57:26 +0800

    week5作業L1

--- week5/routes/creditPackage.js
+++ week5/routes/creditPackage.js
@@ -1,17 +1,103 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
-const { dataSource } = require('../db/data-source')
-const logger = require('../utils/logger')('CreditPackage')
+const router = express.Router();
+const { dataSource } = require("../db/data-source");
+const logger = require("../utils/logger")("CreditPackage");
+const {
+  isUndefined,
+  isNotValidString,
+  isNotValidInteger,
+} = require("../utils/verification");
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
+  try {
+    const creditPackage = await dataSource.getRepository("CreditPackage").find({
+      select: ["id", "name", "credit_amount", "price"],
+    });
+    res.status(200).json({
+      status: "success",
+      data: creditPackage,
+    });
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+});
 
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, credit_amount: creditAmount, price } = req.body;
+    if (
+      isUndefined(name) ||
+      isNotValidString(name) ||
+      isUndefined(creditAmount) ||
+      isNotValidInteger(creditAmount) ||
+      isUndefined(price) ||
+      isNotValidInteger(price)
+    ) {
+      res.status(400).json({
+        status: "failed",
+        message: "欄位未填寫正確",
+      });
+      return;
+    }
+    const creditPackageRepo = await dataSource.getRepository("CreditPackage");
+    const doublecreditPackage = await creditPackageRepo.find({
+      where: {
+        name,
+      },
+    });
+    if (doublecreditPackage.length > 0) {
+      res.status(409).json({
+        status: "failed",
+        message: "資料重複",
+      });
+      return;
+    }
+    const newcreditPackage = await creditPackageRepo.create({
+      name,
+      credit_amount: creditAmount,
+      price,
+    });
+    const result = await creditPackageRepo.save(newcreditPackage);
+    res.status(200).json({
+      status: "success",
+      data: result,
+    });
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-})
+router.delete("/:creditPackageId", async (req, res, next) => {
+  try {
+    const { creditPackageId } = req.params;
+    if (isUndefined(creditPackageId) || isNotValidString(creditPackageId)) {
+      res.status(400).json({
+        status: "failed",
+        message: "欄位未填寫正確",
+      });
+      return;
+    }
+    const result = await dataSource
+      .getRepository("CreditPackage")
+      .delete(creditPackageId);
+    if (result.affected === 0) {
+      res.status(400).json({
+        status: "failed",
+        message: "ID錯誤",
+      });
+      return;
+    }
+    res.status(200).json({
+      status: "success",
+      data: result,
+    });
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+});
 
-router.delete('/:creditPackageId', async (req, res, next) => {
-})
-
-module.exports = router
+module.exports = router;
